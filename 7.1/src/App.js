b4c6b968f96ab213216186416963dd9c45bb8db7
mika <mikagivner123@gmail.com>
2023-02-15 11:46:49 +0200

    new

--- 7.1/src/App.js
+++ 7.1/src/App.js
@@ -1,45 +1,27 @@
 import CustomButton from "./components/CustomButton";
 import React, { useState } from "react";
 
+const colors = ["blue", "red", "yellow"];
 function App() {
-  const colors = ["blue", "red", "yellow"];
   const [whichColor, setWhichColor] = React.useState("");
   function resetColor(e) {
     if (e.target.innerText === "red") setWhichColor("red");
     else if (e.target.innerText === "blue") setWhichColor("blue");
     else if (e.target.innerText === "yellow") setWhichColor("yellow");
   }
-  // return (
-  //   <div>
-  //     {colors.forEach((x) => {
-  //       <CustomButton
-  //         onClick={resetColor}
-  //         className={x}
-  //         color={x}
-  //       ></CustomButton>;
-  //     })}
-  //   </div>
-  // );
   return (
     <div>
-      <CustomButton
-        onClick={resetColor}
-        className={colors[0]}
-        color={colors[0]}
-      ></CustomButton>
-      <CustomButton
-        onClick={resetColor}
-        className={colors[1]}
-        color={colors[1]}
-      ></CustomButton>
-      <CustomButton
-        onClick={resetColor}
-        className={colors[2]}
-        color={colors[2]}
-      ></CustomButton>
+      {colors.map((x) => {
+        return (
+          <CustomButton
+            onClick={resetColor}
+            className={x}
+            color={x}
+          ></CustomButton>
+        );
+      })}
       <div>The color selected is:{whichColor}</div>
     </div>
   );
 }
-
 export default App;
